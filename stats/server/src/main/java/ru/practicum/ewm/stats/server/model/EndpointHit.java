5a1860b0b3d5d877b9794b532c6735b6ca12d5b4
mmsobakin <mmsobakin@spasibosb.ru>
2024-12-16 14:18:00 +0300

    first

---
+++ stats/server/src/main/java/ru/practicum/ewm/stats/server/model/EndpointHit.java
@@ -0,0 +1,31 @@
+package ru.practicum.ewm.stats.server.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "hits")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class EndpointHit {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String app;
+
+    @Column(nullable = false)
+    private String uri;
+
+    @Column(nullable = false, length = 15)
+    private String ip;
+
+    @Column(nullable = false)
+    private LocalDateTime timestamp;
+}
